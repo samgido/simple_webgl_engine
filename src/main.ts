030288d106aad09b79ba99b1e0223c1d9b9cf201
Samuel Jackson Gido <132026917+samgido@users.noreply.github.com>
2025-09-06 16:32:58 -0500

    add uniform functionality

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@
 import vertexShaderSource from './resources/shaders/shader.vert'; //--loader:.vert=text flag 
 import fragmentShaderSource from './resources/shaders/shader.frag'; //--loader:.frag=text flag 
 
-import { initSetupFunctions } from './webgl_utils'
+import { createWebGL2Renderer } from './webgl_utils'
 
 function main() {
-  const webglUtils = initSetupFunctions("canvas", [vertexShaderSource, fragmentShaderSource]);
+  const renderer = createWebGL2Renderer("canvas", [vertexShaderSource, fragmentShaderSource]);
 
   //Format: clip.u, clip.v, tex.u, tex.v
   const shapeData = [
@@ -18,22 +18,25 @@ function main() {
     0.5, -0.5, 1., 0.,
     0.5, 0.5, 1., 1.
   ];
-  webglUtils.loadShapeDataBuffer(shapeData);
+  renderer.loadShapeDataBuffer(shapeData);
+
+  //Uniforms
+  const numSegmentsUniform = renderer.createUniform("num_segments", 'int', 3);
 
   //Load image from server as texture
   //Create temporary texture to render while source is downloading
-  const texture = webglUtils.createTemporaryTexture();
+  const texture = renderer.createTemporaryTexture();
 
   const image = new Image();
 
   //Async handle texture download
   image.src = "http://localhost:3000/src/resources/textures/wall.jpg"; //Change
   image.addEventListener("load", function () {
-    webglUtils.loadImageIntoTexture(texture, image);
-    webglUtils.drawScene();
+    renderer.loadImageIntoTexture(texture, image);
+    renderer.drawScene();
   });
 
-  webglUtils.drawScene();
+  renderer.drawScene();
 }
 
 main();
